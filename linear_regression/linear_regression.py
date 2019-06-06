89f3b36de5d0f0e61335d0e5c976cf288f142f73
juliendelannoy <julien.delannoy@ext.adeo.com>
2019-06-06 11:20:51 +0200

    extract test

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -3,17 +3,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def mock_data(m : int):
-    x = np.random.rand(m)
-    y = mock_func_linear(x)
-    x = x * 300
-    return x, y
-
-def mock_func_linear(value):
-    y = value * 300
-    rand_y = np.random.rand(value.size)*25
-    return  y + rand_y + 30
-
 def init_theta(i : int):
     return np.random.rand(i)
 
@@ -37,33 +26,3 @@ def gradient_descent(x, y, theta, learning_rate): #learning rate : alpha
 def scaler(z):
     pass
 
-m = 20
-
-theta = init_theta(2)
-
-x,y = mock_data(m)
-
-thetas = []
-costs = []
-i=0
-while i < 200:
-    print(cost_function(hyp_func(x, theta), y))
-    theta = gradient_descent(x, y, theta, 0.1)
-    plt.scatter(x, y)
-    plt.scatter(x,hyp_func(x, theta))
-    print(theta)
-    
-    thetas.append(theta)
-    costs.append(cost_function(hyp_func(x, theta),y))
-    costss = np.array(costs)
-    thetass = np.array(thetas)
-    plt.plot(costss)
-    i +=1 
-    
-y_h = hyp_func(x, theta)
-plt.scatter(x, y)
-plt.scatter(x,y_h)
-
-
-plt.plot(thetass[:,0],thetass[:,1])
-plt.plot(costss)
