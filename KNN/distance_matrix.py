9aa22d31c9702eb19169d28276310133d9c99840
Jivelien <julien.Delannoy@hotmail.com>
2020-02-27 22:50:22 +0100

    return list of distinct value

--- KNN/distance_matrix.py
+++ KNN/distance_matrix.py
@@ -1,10 +1,15 @@
 class DistanceMatrix:
     def __init__(self):
-        self.label = []
+        self.distances = []
 
     def add_distance(self, distance, label):
-        self.label = [label]
+        self.distances.append(self.distance_point(distance, label))
 
     def find_nearest_label(self, k):
-        return self.label
-    # or list of label ?
+        nearest = [distance_point.label for distance_point in self.distances]
+        return list(dict.fromkeys(nearest))
+
+    class distance_point:
+        def __init__(self,distance,label):
+                self.distance=distance
+                self.label=label
--- KNN/test_distance_matrix.py
+++ KNN/test_distance_matrix.py
@@ -22,6 +22,27 @@ class TestDistanceMatrix(unittest.TestCase):
 
         self.assertEqual(result, expected)
 
+    def test_find_nearest_with_two_different_labels(self):
+        distance_matrix = DistanceMatrix()
+
+        distance_matrix.add_distance(3.8, 'blue')
+        distance_matrix.add_distance(3.8, 'red')
+
+        result = distance_matrix.find_nearest_label(k=3)
+        expected = ['blue','red']
+
+        self.assertEqual(result, expected)
+
+    def test_find_nearest_with_two_identical_labels(self):
+        distance_matrix = DistanceMatrix()
+
+        distance_matrix.add_distance(3.8, 'blue')
+        distance_matrix.add_distance(3, 'blue')
+
+        result = distance_matrix.find_nearest_label(k=3)
+        expected = ['blue']
+
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
