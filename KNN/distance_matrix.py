97155ebdaafbd35f00a19196b52dddacb850ceb2
Jivelien <julien.Delannoy@hotmail.com>
2020-02-28 00:19:10 +0100

    workable extractor for label distance but dirty, next step must be cleaning

--- KNN/distance_matrix.py
+++ KNN/distance_matrix.py
@@ -6,16 +6,30 @@ class DistanceMatrix:
         self.distances.append(self.distance_point(distance, label))
 
     def find_nearest_label(self, k):
-        sorted(self.distances,key=lambda l:l.distance)
-
-        max = self.distances[0].distance
+        self.distances = sorted(self.distances,key=lambda d:d.distance, reverse=False)
+        min = self.distances[0].distance
         result = []
         for distance in self.distances:
-            if distance.distance == max:
+            if distance.distance == min:
                 result.append(distance.label)
             else:
+                k-=1
+                if k == 0:
+                    break
+                result.append(distance.label)
+                min = distance.distance
+
+        result_set = [[x,result.count(x)] for x in set(result)]
+        result_set = sorted(result_set, key=lambda l:l[1], reverse = True)
+        max = result_set[0][1]
+        final_result=[]
+        for r in result_set:
+            if r[1] == max:
+                        final_result.append(r[0])
+            else:
                 break
-        return result
+
+        return final_result
 
     class distance_point:
         def __init__(self,distance,label):
--- KNN/test_distance_matrix.py
+++ KNN/test_distance_matrix.py
@@ -5,10 +5,10 @@ class TestDistanceMatrix(unittest.TestCase):
     def test_find_nearest_with_one_circle_point(self):
         distance_matrix = DistanceMatrix()
 
-        distance_matrix.add_distance(3.8, 'circle')
+        distance_matrix.add_distance(3.8, 'blue')
 
         result = distance_matrix.find_nearest_label(k=1)
-        expected = ['circle']
+        expected = ['blue']
 
         self.assertEqual(result, expected)
 
@@ -30,8 +30,8 @@ class TestDistanceMatrix(unittest.TestCase):
 
         result = distance_matrix.find_nearest_label(k=3)
         expected = ['blue','red']
-
-        self.assertEqual(result, expected)
+        #FIXME should exist a better method
+        self.assertEqual(sorted(result), sorted(expected))
 
     def test_find_nearest_with_two_identical_labels(self):
         distance_matrix = DistanceMatrix()
@@ -51,9 +51,35 @@ class TestDistanceMatrix(unittest.TestCase):
         distance_matrix.add_distance(3, 'blue')
 
         result = distance_matrix.find_nearest_label(k=1)
+        expected = ['blue']
+
+        self.assertEqual(result, expected)
+
+    def test_find_most_present_label_between_three(self):
+        distance_matrix = DistanceMatrix()
+
+        distance_matrix.add_distance(3.8, 'red')
+        distance_matrix.add_distance(3.8, 'red')
+        distance_matrix.add_distance(3, 'blue')
+
+        result = distance_matrix.find_nearest_label(k=3)
         expected = ['red']
 
         self.assertEqual(result, expected)
 
+
+    def test_find_most_present_ambigous_labels_between_four(self):
+        distance_matrix = DistanceMatrix()
+
+        distance_matrix.add_distance(3.8, 'red')
+        distance_matrix.add_distance(3.8, 'red')
+        distance_matrix.add_distance(3, 'blue')
+        distance_matrix.add_distance(3, 'blue')
+
+        result = distance_matrix.find_nearest_label(k=3)
+        expected = ['blue','red']
+
+        self.assertEqual(sorted(result), sorted(expected))
+        
 if __name__ == '__main__':
     unittest.main()
