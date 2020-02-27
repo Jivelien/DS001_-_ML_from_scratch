d155f13614f2baeaf8ff95bb70f96a230ed36883
Jivelien <julien.Delannoy@hotmail.com>
2020-02-28 00:33:33 +0100

    rename KNN to dataset

--- KNN/Dataset.py
+++ KNN/Dataset.py
@@ -1,14 +1,11 @@
-class KNN:
+class Dataset:
     def __init__(self):
-        self.dataset = []
+        self.points = []
 
     def add_point(self, point):
-        self.dataset.append(point)
+        self.points.append(point)
 
     def add_set_point(self, list_points):
         for point in list_points:
             self.add_point(point)
 
-    def x(self, point):
-        label=''
-        return label
