1bd78e2d38044c42fd53bf4b9aea7e7ebeb5cce6
Jivelien <julien.Delannoy@hotmail.com>
2020-02-28 15:50:02 +0100

    rename

---
+++ KNN/KNN_model.py
@@ -0,0 +1,20 @@
+from distance_matrix import DistanceMatrix
+from dataset import Dataset
+from point_2d import Point2D
+
+class KNN:
+    def __init__(self, k, dataset=None):
+        self.k = k
+        self.dataset = Dataset()
+
+    def find_label_of_a_point(self, point):
+        distance_matrix = self._compute_distance_matrix(point)
+        return distance_matrix.find_nearest_label(self.k)
+
+    def _compute_distance_matrix(self, point_to_match):
+        distance_matrix = DistanceMatrix()
+        for point in self.dataset.points:
+            distance = point_to_match.euclidean_distance(point)
+            distance_matrix.add_distance(distance, point.label)
+        return distance_matrix
+
