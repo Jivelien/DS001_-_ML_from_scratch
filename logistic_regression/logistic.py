7a9b612f288a7cff227fb197323edcf5764b275e
DELANNOY <jdelannoy@ceetrus.com>
2020-08-10 14:02:55 +0200

    add todo as note

--- logistic_regression/logistic.py
+++ logistic_regression/logistic.py
@@ -3,13 +3,16 @@ from math import exp
 from matplotlib import pyplot as plt
 import numpy as np
 
-def sigmoid_function(x, theta):
-    return 1 /( 1 + exp(-theta*x) )
+#TODO extract theta notion from sigmoid function
+def sigmoid_function(x, theta0):
+    return 1 /( 1 + exp(-theta0*x) )
 
-def create_point_of_curve(value_list: list, theta):
-    return [sigmoid_function(v, theta) for v in value_list] 
+def create_point_of_curve(value_list: list, theta0):
+    return [sigmoid_function(v, theta0) for v in value_list] 
 
-
+def decision_boundary(value, threshold = 0.5):
+    return value >= 0.5
+    
 test = np.arange(-1,1,0.1)
 test_sigm = create_point_of_curve(test, 10)
 
