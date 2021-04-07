03c5142b4463ede95f84f10ac4a951923280daab
Jivelien <julien.Delannoy@hotmail.com>
2021-04-07 14:14:41 +0200

    clean useless code

--- KNN/distance_matrix.py
+++ KNN/distance_matrix.py
@@ -33,31 +33,12 @@ class DistanceMatrix:
     def keep_neighbour_egal_distance(self, distance, matrix):
         return [x for x in matrix if x.distance == distance]
 
-    def select_k_nearest_neighbour(self, k):
-        sorted_distances = self._get_sorted_distance_matrix()
-        nearest_neighbour_subset = self.keep_k_nearest_neighbour(k, sorted_distances)
-        max_distance_in_nn_subset = nearest_neighbour_subset[-1].distance
-        additionnal_nearest_neighbour = self.keep_neighbour_egal_distance(max_distance_in_nn_subset, sorted_distances[min(k, len(sorted_distances)):])
-        nearest_neighbour_subset += additionnal_nearest_neighbour
-        return nearest_neighbour_subset
-
 
     def select_label_in_the_majority(self, labels):
         labels_counter = collections.Counter(labels).most_common()
         max_count_label = labels_counter[0][1]
         result = [ label[0] for label in labels_counter if label[1] == max_count_label ]
         return result
-
-
-    def find_nearest_label_refact(self, k):
-        if self._is_matrix_empty():
-            return []
-        nearest_neighbour_subset = self.select_k_nearest_neighbour(k)
-        
-        # TODO : try to refacto with rank : [sorted(l).index(x) + 1 for x in l]  
-        
-        result_labels = [ point.label for point in nearest_neighbour_subset]
-        return self.select_label_in_the_majority(result_labels)
 
 
     def find_nearest_label_refact_by_ranking(self, k):
@@ -80,36 +61,3 @@ class DistanceMatrix:
 
         for index in range(len(self.distance_matrix)):
                 self.distance_matrix[index].rank = rank_per_index[index]
-
-    def find_nearest_label_ori(self, k):
-        if self._is_matrix_empty():
-            return []
-
-        sorted_distance = sorted(self.distance_matrix, key=lambda d: d.distance, reverse=False)
-        #PART 1
-        # prendre k element de la list
-        # si k+1 meme distance, prendre
-        min = sorted_distance[0].distance
-        result = []
-        for distance in sorted_distance:
-            if distance.distance == min:
-                result.append(distance.label)
-            else:
-                k -= 1
-                if k == 0:
-                    break
-                result.append(distance.label)
-                min = distance.distance
-
-        #PART 2
-        result_set = [[x,result.count(x)] for x in set(result)]
-        result_set = sorted(result_set, key=lambda l: l[1], reverse=True)
-        max = result_set[0][1]
-        final_result=[]
-        for r in result_set:
-            if r[1] == max:
-                final_result.append(r[0])
-            else:
-                break
-
-        return final_result
