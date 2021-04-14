a20678b3a8449708ac58fc07c8f06856f1bde398
Jivelien <julien.Delannoy@hotmail.com>
2021-04-14 14:06:26 +0200

    lint

--- KNN/distance_matrix.py
+++ KNN/distance_matrix.py
@@ -14,7 +14,6 @@ class DistanceMatrix:
         self.distance_matrix : List[DistanceMatrix.DistancePoint]= []
 
     def add_distance(self, distance, label):
-        #TODO correct add_distance ; maybe rank at this moment ?
         self.distance_matrix.append(self.DistancePoint(distance=distance, label=label, rank=-1))
 
     def _is_matrix_empty(self):
@@ -26,13 +25,11 @@ class DistanceMatrix:
     def find_nearest_label(self, k):
          return self.find_nearest_label_refact_by_ranking(k)
 
-
     def keep_k_nearest_neighbour(self, k, sorted_distance_matrix):
         return sorted_distance_matrix[0:min(k, len(sorted_distance_matrix))] 
 
     def keep_neighbour_egal_distance(self, distance, matrix):
         return [x for x in matrix if x.distance == distance]
-
 
     def select_label_in_the_majority(self, labels):
         labels_counter = collections.Counter(labels).most_common()
@@ -40,18 +37,15 @@ class DistanceMatrix:
         result = [ label[0] for label in labels_counter if label[1] == max_count_label ]
         return result
 
-
     def find_nearest_label_refact_by_ranking(self, k):
         if self._is_matrix_empty():
             return []
         result_labels = self.select_k_nearest_neighbour_by_ranking(k)
         return self.select_label_in_the_majority(result_labels)
 
-
     def select_k_nearest_neighbour_by_ranking(self, k):
         self.rank_all_labels()
         return [distance_point.label for distance_point in self.distance_matrix if distance_point.rank <= k ]
-
 
     def rank_all_labels(self) -> None:
         self._get_sorted_distance_matrix()
